e50ab95f6f88fe0d8f06ebd738adc4dbd624b900
Tchr1s <csepulveda232001@gmail.com>
2025-04-26 03:02:45 -0400

    contact form working

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
         document.getElementById('menu-toggle').checked = false;
-    });
+    });    
 });
+
+
+window.onload = function() {
+    // Reset the form fields when the page loads
+    document.getElementById("form").reset();
+};
+
